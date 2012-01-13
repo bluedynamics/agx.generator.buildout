3f6c593922bec2ace674594e95d6ab7510257a42
Robert Niederreiter <rnix@squarewave.at>
2012-01-13 10:38:04 +0100

    initial

---
+++ README.rst
@@ -0,0 +1,1 @@
+Welcome to agx.generator.buildout
---
+++ setup.py
@@ -0,0 +1,42 @@
+# 
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = "1.0"
+shortdesc =""
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(name="agx.generator.buildout",
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+          "",
+      ],
+      keywords="",
+      author="",
+      author_email="",
+      url="",
+      license="",
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      namespace_packages=["agx", "agx.generator"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          ##code-section dependencies
+          ##/code-section dependencies
+      ],
+      extras_require = dict(
+          ##code-section extras_require
+          ##/code-section extras_require
+      ),
+      entry_points="""
+      ##code-section entry_points
+      ##/code-section entry_points
+      """,
+      ##code-section additionals
+      ##/code-section additionals
+      )
